0b67feda53e9ff6fe05eeef9225cfdfa934ebc99
ashiq611 <72414858+ashiq611@users.noreply.github.com>
2023-11-09 23:01:54 +0600

    fix

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("db_videos.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const port = process.env.PORT || 5173; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
